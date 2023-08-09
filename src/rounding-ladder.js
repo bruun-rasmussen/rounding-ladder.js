bc2a69e01619065ab1ef2d2db4e595a111ddd6b1
Adam Lett <adamlett@gmail.com>
2023-08-09 16:54:56 +0200

    Initial commit

---
+++ src/rounding-ladder.js
@@ -0,0 +1,130 @@
+/**
+ * Always round towards plus infinity
+ */
+export const CEIL = (amount, stepBelow, stepAbove) => {
+	if (stepBelow === amount) {
+		return stepBelow
+	} else {
+		return stepAbove
+	}
+}
+/**
+ * Always round towards minus infinity
+ */
+export const FLOOR = (amount, stepBelow, stepAbove) => {
+	if (stepAbove === amount) {
+		return stepAbove
+	} else {
+		return stepBelow
+	}
+}
+/**
+ * Rounds to the nearest step in the ladder, but if the number is exactly between two steps, it rounds towards
+ * plus infinity.
+ */
+export const HALF_UP = (val, stepBelow, stepAbove) => {
+	if (Math.abs(val - stepBelow) < Math.abs(val - stepAbove)) {
+		return stepBelow
+	} else {
+		return stepAbove
+	}
+}
+/**
+ * Banker's rounding is a way of rounding intended to minimize the bias in the rounding process.
+ * To accomplish this, it rounds towards the nearest even-indexed step if the number is exactly between two steps.
+ */
+export const BANKERS_ROUNDING = (amount, stepBelow, stepAbove, stepIndex) => {
+	const halfway = (stepBelow + stepAbove) / 2
+	if (amount < halfway) return stepBelow
+	if (amount > halfway) return stepAbove
+	if (stepIndex % 2 === 0) {
+		return stepBelow
+	} else {
+		return stepAbove
+	}
+}
+/**
+ * Rounds positive numbers to the nearest step on a logarithmic ladder of numbers
+ */
+export default class Rounder {
+	decade
+	base
+	roundingStrategy
+	/**
+	 * Constructs a new Rounder
+	 *
+	 * @param decade the steps for the first segment of the ladder, in ascending order
+	 * @param base the base of the logarithmic ladder. Each segment of the ladder is equal to the previous segment multiplied with this number.
+	 * @param roundingStrategy the strategy used when the number is between two steps to select which of the two
+	 * steps to use. Defaults to BANKERS_TIE_BREAKER.
+	 */
+	constructor(decade, base = 10, roundingStrategy = BANKERS_ROUNDING) {
+		if (decade.length === 0) throw new Error('Decade must contain at least one step')
+		// If the last step of the decade is larger than or equal to the first step multiplied by the base, it would
+		// break the invariant that each step on the ladder must be larger than the prior step.
+		if (decade[decade.length - 1] >= decade[0] * base) {
+			throw new Error(
+				'The last step of the decade must be smaller than the first step multiplied by the base'
+			)
+		}
+		this.decade = decade
+		this.base = base
+		this.roundingStrategy = roundingStrategy
+	}
+	/**
+	 * Rounds to the nearest step in the ladder using the configured rounding strategy to break ties.
+	 */
+	round(amount) {
+		return this.search(amount, this.roundingStrategy)
+	}
+	/**
+	 * Finds the step in the ladder closest to plus infinity that is smaller than or equal to the given number.
+	 */
+	floor(amount) {
+		return this.search(amount, FLOOR)
+	}
+	/**
+	 * Finds the step in the ladder closest to minus infinity that is larger than or equal to the given number.
+	 */
+	ceil(amount) {
+		return this.search(amount, CEIL)
+	}
+	search(amount, roundingStrategy = this.roundingStrategy) {
+		if (amount === 0) return 0
+		const [multiplier, segmentIndex] = this.findMultiplier(amount)
+		const currentDecade = [
+			...this.decade.map((step) => step * multiplier),
+			this.decade[0] * multiplier * this.base
+		]
+		let i = 0
+		while (i < currentDecade.length) {
+			i += 1
+			if (amount <= currentDecade[i]) {
+				break
+			}
+		}
+		return roundingStrategy(
+			amount,
+			currentDecade[i - 1],
+			currentDecade[i],
+			i + segmentIndex * this.decade.length
+		)
+	}
+	findMultiplier(amount) {
+		let multiplier = 1
+		let segmentIndex = 0
+		let nextMultiplier = this.base
+		let previousMultiplier = 1
+		while (amount < this.decade[0] * multiplier) {
+			multiplier = previousMultiplier
+			previousMultiplier /= this.base
+			segmentIndex -= 1
+		}
+		while (amount >= nextMultiplier * this.decade[0]) {
+			multiplier = nextMultiplier
+			nextMultiplier *= this.base
+			segmentIndex += 1
+		}
+		return [multiplier, segmentIndex]
+	}
+}
---
+++ tests/rounding-ladder.spec.ts
@@ -0,0 +1,105 @@
+import { describe, expect, it } from '@jest/globals'
+import Rounder, { BANKERS_ROUNDING } from '../src/rounding-ladder'
+
+describe('rounding ladder', () => {
+	const decade = [10, 11, 13, 15, 17, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90]
+	const ladder = new Rounder(decade)
+
+	describe('construction', () => {
+		it('throws an error if the decade is empty', () => {
+			expect(() => new Rounder([], 10)).toThrowError('Decade must contain at least one step')
+		})
+
+		it('throws an error if the last step of the decade is larger than or equal to the first step multiplied by the base', () => {
+			expect(() => new Rounder([1, 2], 2)).toThrowError(
+				'The last step of the decade must be smaller than the first step multiplied by the base'
+			)
+		})
+	})
+
+	describe('if the number is zero', () => {
+		it('floors to zero', () => {
+			expect(ladder.floor(0)).toBe(0)
+		})
+
+		it('ceils to zero', () => {
+			expect(ladder.ceil(0)).toBe(0)
+		})
+
+		it('rounds to zero', () => {
+			expect(ladder.round(0)).toBe(0)
+		})
+	})
+
+	describe('if the number is less than the first step', () => {
+		it('rounds to the nearest step that is a base root of a step in the decade', () => {
+			expect(ladder.round(2.2)).toEqual(2)
+		})
+	})
+
+	describe('if the number is equal to a step', () => {
+		it('floors to the step', () => {
+			expect(ladder.floor(10)).toEqual(10)
+		})
+
+		it('ceils to the step', () => {
+			expect(ladder.ceil(10)).toEqual(10)
+		})
+
+		it('rounds to the step', () => {
+			expect(ladder.round(10)).toEqual(10)
+		})
+	})
+
+	describe('if the number is between two steps', () => {
+		describe('and the number is closer to the lower step', () => {
+			it('rounds down', () => {
+				expect(ladder.round(18)).toEqual(17)
+			})
+		})
+
+		describe('and the number is closer to the higher step', () => {
+			it('rounds up', () => {
+				expect(ladder.round(19)).toEqual(20)
+			})
+		})
+
+		describe('and the number is exactly halfway between the two steps', () => {
+			describe("when using banker's rounding", () => {
+				const evenLadder = new Rounder([10, 20, 40, 60], 10, BANKERS_ROUNDING)
+
+				it('it rounds towards plus infinity if the index of the step is even, and towards minus infinity if the index of the step is odd', () => {
+					expect(evenLadder.round(8)).toBe(6)
+					expect(evenLadder.round(15)).toBe(20)
+					expect(evenLadder.round(30)).toBe(20)
+					expect(evenLadder.round(50)).toBe(60)
+					expect(evenLadder.round(80)).toBe(60)
+					expect(evenLadder.round(150)).toBe(200)
+				})
+
+				describe('a ladder with an odd numbered steps in the decade, behaves like a ladder with an even number of steps in the decade', () => {
+					const oddDecade = [10, 30, 60]
+					const oddLadder = new Rounder(oddDecade, 10, BANKERS_ROUNDING)
+					const evenDecade = [10, 30, 60, 100, 300, 600]
+					const evenLadder = new Rounder(evenDecade, 100, BANKERS_ROUNDING)
+
+					it('behaves like it would if it were expressed as a ladder with an even number of steps in the decade and the base squared', () => {
+						;[5, 20, 45, 80, 200, 450, 800].forEach((number) => {
+							expect(oddLadder.round(number)).toBe(evenLadder.round(number))
+						})
+					})
+				})
+			})
+		})
+
+		describe('when the number is exactly between the last step of the decade, and the first step of the next decade', () => {
+			it('floors to the last step of the decade', () => {
+				expect(ladder.floor(95)).toBe(90)
+			})
+
+			it('ceils to the first step of the next decade', () => {
+				expect(ladder.ceil(95)).toBe(100)
+			})
+		})
+	})
+})
